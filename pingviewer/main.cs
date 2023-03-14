d51fc09e7558bc6be8666fdb22b86ebdcac93ab4
HK_Shuttle <52975544+HKShuttle@users.noreply.github.com>
2023-03-15 00:25:37 +0900

    プロジェクト ファイルを追加します。

---
+++ pingviewer/main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace pingviewer
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ping to {0}, 1 block = 2ms", args[0]);
+            Ping ping = new Ping();
+            for (int i = 0; i < 1000; i++)
+            {
+                PingReply reply = ping.Send(args[0]);
+                if(reply.Status == IPStatus.Success)
+                {
+                    var sb = new StringBuilder();
+                    for (int j = 0; j < reply.RoundtripTime; j += 2)
+                    {
+                        sb.Append("#");
+                    }
+                    Console.WriteLine(sb.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("X");
+                }
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
