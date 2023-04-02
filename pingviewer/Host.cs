083bbb6ade10368fde9a1146696dc0b04c4a69b1
HK_Shuttle <52975544+HKShuttle@users.noreply.github.com>
2023-04-02 22:09:25 +0900

    <wip> Hostクラスを実装

---
+++ pingviewer/Host.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using pingviewer.lib;
+using System.Globalization;
+
+namespace pingviewer
+{
+    internal class Host
+    {
+        private string hostname;
+        private LongRingBuffer successRTT;
+        private int count;
+        private int success;
+        private int fail;
+        private Ping ping;
+
+        public int Count { get; private set; }
+        public int Success { get; private set; }
+        public int Fail { get; private set; }
+        public string Hostname { get; private set; }
+        public LongRingBuffer SuccessRTT { get; private set; }
+
+        public Host(string hostname)
+        {
+            this.hostname = hostname;
+            ping = new Ping();
+            count = 0;
+            success = 0;
+            fail = 0;
+            successRTT = new LongRingBuffer(1000);
+        }
+
+        public PingReply SendPing()
+        {
+            return ping.Send(hostname);
+        }
+    }
+}
